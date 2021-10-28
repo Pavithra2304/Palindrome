576b1c0a45b50a7ca8175fa2988526cf6f333a80
Pavithra2304 <92969170+Pavithra2304@users.noreply.github.com>
2021-10-28 17:06:45 +0530

    Add files via upload

---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+	class Program
+	{
+		public static void palindrome()
+		{
+			string s, revs = "";
+			Console.WriteLine(" Enter string");
+			s = Console.ReadLine();
+			for (int i = s.Length - 1; i >= 0; i--)
+			{
+				revs += s[i].ToString();
+			}
+			if (revs == s)
+			{
+				Console.WriteLine("String is Palindrome");
+			}
+			else
+			{
+				Console.WriteLine("String is not Palindrome");
+			}
+			Console.Read();
+		}
+		static void Main(string[] args)
+		{
+			palindrome();
+		}
+	}
+}
